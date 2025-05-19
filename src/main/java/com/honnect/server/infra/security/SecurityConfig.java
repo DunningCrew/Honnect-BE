372366747455e050b5dcfddad90c25d7c1629749
OSS-LDH <ukki5782@gmail.com>
2025-05-19 13:58:09 +0900

    feat:SecurityConfig추가

---
+++ src/main/java/com/honnect/server/infra/security/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.honnect.server.infra.security;
+
+import com.honnect.server.infra.security.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+
+                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        // 인증 없이 접근 가능한 엔드포인트
+                        .requestMatchers(
+                                "/register" , "/login"
+                        )
+                        .permitAll() // 인증 없이 접근 허용
+                        .anyRequest().authenticated() // 그 외의 모든 요청은 인증 필요
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
